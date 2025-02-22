0130e71c47766bb4f3f5d498cc18210802c4ee96
civilblur <git.webd@extraramen.com>
2025-02-22 02:41:02 +0100

    refactor: major refactor to add funtionality and styliing to the youlagTheaterModal

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const modalContainerClassName = `youlag-theater-modal-container`;
-const modalContentClassName = `youlag-theater-modal-container`;
+const modalContentClassName = `youlag-theater-modal-content`;
 const modalCloseIdName = `youlagCloseModal`;
 const modalToggleFavoriteIdName = `youlagToggleFavorite`;
+const modalFavoriteClassName = `youlag-favorited`;
 
 function handleActiveRssItem(targetOrEvent) {
   // Coordinates the event for extracting the data triggering.
@@ -12,14 +13,24 @@ function handleActiveRssItem(targetOrEvent) {
     feedItem = targetOrEvent.closest('div[data-feed]');
   }
   if (!feedItem) return;
+  disableBodyScroll(true);
   const data = extractFeedItemData(feedItem);
+  data.feedItemEl = feedItem;
   createModalWithData(data);
 }
 
 function extractFeedItemData(feedItem) {
   // Extract data from the provided target element.
+  const extractedYoutubeUrl = feedItem.querySelector('.enclosure-content a[href*="youtube"]')?.href || '';
+  const youtubeUrl = extractedYoutubeUrl.replace('/v/', '/watch?v=').replace('?version=3', '');
+  const youtubeEmbedUrl = youtubeUrl.replace('/watch?v=', '/embed/');
+  const authorElement = feedItem.querySelector('.flux_header');
+  const authorFilterElement = authorElement?.querySelector('.website a.item-element[href*="get=f_"]');
+
   return {
-    author: feedItem.querySelector('.flux_header')?.getAttribute('data-article-authors') || '',
+    author: authorElement?.getAttribute('data-article-authors') || '',
+    author_filter_url: authorFilterElement?.href || '',
+    favicon: feedItem.querySelector('img.favicon')?.src || '',
     favorite_toggle_url: feedItem.querySelector('a.item-element.bookmark')?.href || '',
     favorited: !feedItem.querySelector('.bookmark img[src*="non-starred"]'),
     thumbnail: feedItem.querySelector('.thumbnail img')?.src || '',
@@ -28,9 +39,11 @@ function extractFeedItemData(feedItem) {
     date: feedItem.querySelector('.flux_content .date')?.textContent.trim() || '',
     video_embed_url: feedItem.querySelector('article.flux_content .text > iframe')?.getAttribute('data-original') || feedItem.querySelector('article iframe')?.getAttribute('src'),
     video_description:
-      '<div>' +
+      '<div class="youlag-video-description-content">' +
         (feedItem.querySelector('.enclosure-description')?.innerHTML.trim() || '') +
-      '</div>'
+      '</div>',
+    video_youtube_url: youtubeUrl,
+    video_youtube_url_embed: youtubeEmbedUrl,
   };
 }
 
@@ -41,60 +54,109 @@ function createModalWithData(data) {
   if (!modal) {
     modal = document.createElement('div');
     modal.id = 'youlagTheaterModal';
-    modal.style.cssText = `
-      z-index: 99999;
-      position: fixed;
-      top: 0;
-      left: 0;
-      height: 100vh;
-      width: 100vw;
-      background: black;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      overflow: auto;
-    `;
-    modal.innerHTML = `<div class="${modalContainerClassName}" style="background: #fff; padding: 20px; max-width: 90%; box-sizing: border-box;"></div>`;
+    modal.innerHTML = `<div class="${modalContainerClassName}"></div>`;
     document.body.appendChild(modal);
   }
 
   // Add content to modal
-  const container = modal.querySelector(modalContainerClassName);
+  const container = modal.querySelector(`.${modalContainerClassName}`);
   container.innerHTML = `
     <div class="${modalContentClassName}">
-      <h2>${data.title}</h2>
-      <div>Author: ${data.author}</div>
-      <div>Date: ${data.date}</div>
-      <div>
-        <a href="#" id="${modalToggleFavoriteIdName}">Toggle Favorite (Currently: ${data.favorited})</a>
-      </div>
-      <img src="${data.thumbnail}" style="max-width: 100%" />
-      <div>
-        <iframe style="height: 337px; width: 600px;" width="600" height="337" 
-                src="${data.video_embed_url}" frameborder="0" allowfullscreen></iframe>
+
+      <div class="youlag-video-header">
+        <button id="${modalCloseIdName}">×</button>
       </div>
 
-      <a href="${data.external_link}" target="_blank">View on original site</a>
-      <a href="${data.video_embed_url}" target="_blank">Embed URL</a>
+      <div class="youlag-video-container">
+        <div class="youlag-thumbnail-container">
+          <img src="${data.thumbnail}" class="youlag-video-thumbnail" />
+        </div>
+        <div class="youlag-iframe-container">
+          <iframe class="youlag-iframe"
+                  src="${data.video_embed_url}" frameborder="0" allowfullscreen></iframe>
+        </div>
+      </div>
 
-      
-      ${data.video_description}
-      <button id="${modalCloseIdName}">Close</button>
+
+      <div class="youlag-video-details">
+
+        <div class="youlag-video-metadata-container">
+          <h2 class="youlag-video-metadata-title">${data.title}</h2>
+          <div class="youlag-video-metadata-panel">
+
+            <section class="youlag-video-author-section">
+              <img src="${data.favicon}" class="youlag-video-metadata-favicon" />
+
+              <div class="yl-flex yl-flex-col">
+                <div class="youlag-video-metadata-author">
+                  <a href="${data.author_filter_url}">${data.author}</a>
+                </div>
+                <div class="youlag-video-metadata-date">${data.date}</div>
+              </div>
+            </section>
+
+            <section class="youlag-video-actions-container">
+              <a href="#" 
+                class="yl-video-action-button ${modalFavoriteClassName} ${modalFavoriteClassName}--${data.favorited}"
+                id="${modalToggleFavoriteIdName}">
+                <div class="youlag-favorited-icon"></div>
+              </a>
+              <a class="yl-video-action-button" href="${data.external_link}" target="_blank">
+                <span>🌐</span><span>Source</span>
+              </a>
+              <a class="yl-video-action-button" href="${data.video_youtube_url}" target="_blank">
+                <span>▶️</span><span>YouTube</span>
+              </a>
+              <a class="yl-video-action-button" href="${data.video_youtube_url_embed}" target="_blank">
+                <span>📺</span><span>YouTube Embed</span>
+              </a>
+            </section>
+
+          </div>
+
+        </div>
+
+
+
+        <div class="youlag-video-description-container">
+          ${data.video_description}
+        </div>
+        
+      </div>
+
     </div>
   `;
-  container.querySelector(modalCloseIdName)?.addEventListener('click', closeModal);
-  container.querySelector(modalToggleFavoriteIdName)?.addEventListener('click', (e) => {
+  container.querySelector(`#${modalCloseIdName}`)?.addEventListener('click', closeModal);
+  container.querySelector(`#${modalToggleFavoriteIdName}`)?.addEventListener('click', (e) => {
     e.preventDefault();
-    toggleFavorite(data.favorite_toggle_url, container);
+    toggleFavorite(data.favorite_toggle_url, container, data.feedItemEl);
   });
 }
 
-function toggleFavorite(url, container) {
+function toggleFavorite(url, container, feedItemEl) {
+  const favoriteButton = container.querySelector(`#${modalToggleFavoriteIdName}`);
+  if (!favoriteButton) return;
+
   fetch(url, { method: 'GET' })
     .then(response => {
       if (response.ok) {
-        const favorited = container.querySelector(modalToggleFavoriteIdName).textContent.includes('true');
-        container.querySelector(modalToggleFavoriteIdName).textContent = `Toggle Favorite (Currently: ${!favorited})`;
+        // Toggle favorite classes and icons
+        const currentlyTrue = favoriteButton.classList.contains(`${modalFavoriteClassName}--true`);
+        const bookmarkIcon = feedItemEl.querySelector('.item-element.bookmark img.icon');
+        favoriteButton.classList.remove(`${modalFavoriteClassName}--${currentlyTrue}`);
+        favoriteButton.classList.add(`${modalFavoriteClassName}--${!currentlyTrue}`);
+
+        if (currentlyTrue) {
+          feedItemEl.classList.remove('favorite');
+          if (bookmarkIcon) {
+            bookmarkIcon.src = '../themes/Mapco/icons/non-starred.svg';
+          }
+        } else {
+          feedItemEl.classList.add('favorite');
+          if (bookmarkIcon) {
+            bookmarkIcon.src = '../themes/Mapco/icons/starred.svg';
+          }
+        }
       } else {
         console.error('Failed to toggle favorite status');
       }
@@ -103,6 +165,7 @@ function toggleFavorite(url, container) {
 }
 
 function closeModal() {
+  disableBodyScroll(false);
   const modal = document.getElementById('youlagTheaterModal');
   if (modal) modal.remove();
 }
@@ -112,18 +175,76 @@ function setupClickListener() {
   
   if (streamContainer) {
     streamContainer.addEventListener('click', (event) => {
+      // Prevent activation if clicked element is inside .flux_header li.
+      // These are the feed item actions buttons.
+      if (event.target.closest('div[data-feed] .flux_header li.manage')) return;
+      if (event.target.closest('div[data-feed] .flux_header li.labels')) return;
+      if (event.target.closest('div[data-feed] .flux_header li.share')) return;
+      if (event.target.closest('div[data-feed] .flux_header li.link')) return;
       const target = event.target.closest('div[data-feed]');
+
       if (target) {
         handleActiveRssItem(event);
+        collapseBackgroundFeedItem(target);
       }
     });
   }
 }
 
-function init() {
-  setupClickListener();
-  console.log('Custom Feed Script Loaded');
+function collapseBackgroundFeedItem(target) {
+  // Workaround: Closes down the original feed item that activates by FreshRSS clickevent.
+  const feedItem = target;
+  let isActive = feedItem.classList.contains('active') && feedItem.classList.contains('current');
+  const iframes = feedItem.querySelectorAll('iframe');
+
+  iframes.forEach(iframe => {
+    // Disable iframes to prevent autoplay
+    const src = iframe.getAttribute('src');
+    if (src) {
+      console.log('iframe replace:', src);
+      iframe.setAttribute('data-original', src);
+      iframe.setAttribute('src', '');
+    }
+  });
+
+  if (isActive) {
+    // Collapse the feed item
+    feedItem.classList.remove('active');
+    feedItem.classList.remove('current');
+  }
 }
 
+function disableBodyScroll(scroll) {
+  document.body.style.overflow = scroll ? 'hidden' : 'auto';
+}
 
-document.addEventListener('DOMContentLoaded', init);
+function init() {
+  setupClickListener();
+  removeYoulagLoadingState();
+}
+
+function removeYoulagLoadingState() {
+  // By default, the youlag css is set to a loading state.
+  // This will remove the loading state when the script is ready.
+  document.body.classList.add('youlag-loaded');
+}
+
+function initFallback() {
+  if (document.readyState === 'complete' || document.readyState === 'interactive') {
+    init();
+  } else {
+    document.addEventListener('DOMContentLoaded', init);
+    window.addEventListener('load', init);
+  }
+}
+
+// Fallback interval check
+const checkInitInterval = setInterval(() => {
+  if (document.readyState === 'complete') {
+    init();
+    clearInterval(checkInitInterval);
+  }
+}, 1000);
+
+// Ensure init runs
+initFallback();
