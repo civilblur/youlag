d011ea28ec1ee5aafa63b9e95be1b920a7f27b58
Inverle <inverle@proton.me>
2025-11-01 22:50:22 +0100

    Port to FreshRSS extension
    Instead of relying on UserJS/UserCSS

---
+++ extension.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+final class YoulagExtension extends Minz_Extension {
+    #[\Override]
+    public function init(): void {
+        Minz_View::appendStyle($this->getFileUrl('theme.css'));
+        Minz_View::appendScript($this->getFileUrl('script.js'));
+
+        FreshRSS_Context::userConf()->theme = 'Mapco';
+        FreshRSS_Context::userConf()->topline_website = 'full';
+        FreshRSS_Context::userConf()->topline_thumbnail = 'landscape';
+        FreshRSS_Context::userConf()->topline_summary = true;
+    }
+}
