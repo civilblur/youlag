c84bf169e69164b21f905807b1d10ea17099ca66
civilblur <git.webd@extraramen.com>
2025-12-23 10:36:20 +0100

    fix: incorrect path for theme and script

--- extension.php
+++ extension.php
@@ -3,8 +3,8 @@
 final class YoulagExtension extends Minz_Extension {
     #[\Override]
     public function init(): void {
-        Minz_View::appendStyle($this->getFileUrl('static/theme.css'));
-        Minz_View::appendScript($this->getFileUrl('static/script.js'));
+        Minz_View::appendStyle($this->getFileUrl('theme.css'));
+        Minz_View::appendScript($this->getFileUrl('script.js'));
 
         FreshRSS_Context::userConf()->theme = 'Mapco';
         FreshRSS_Context::userConf()->topline_website = 'full';
