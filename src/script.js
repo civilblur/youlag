ae188359f38632544891009a3ddcf2e0c20bcf67
civilblur <git.webd@extraramen.com>
2026-01-16 01:13:44 +0100

    fix: reflect video label setting changes in frontend in settings by applying the labels immediately

--- src/script.js
+++ src/script.js
@@ -83,6 +83,22 @@ function youlagSettingsPageEventListeners() {
     youtubeRadio.addEventListener('change', updateRequired);
     updateRequired();
   }
+
+  const videoLabelCheckbox = document.getElementById('yl_video_labels_enabled');
+  if (videoLabelCheckbox) {
+    // Do an initial check whether video label setting is enabled or not, as the 
+    // DOM injected `data-yl-video-labels` is not available on the settings page.
+    const videoLabelIsChecked = videoLabelCheckbox ? videoLabelCheckbox.checked : false;
+    if (videoLabelIsChecked) {
+      document.body.classList.add('youlag-video-labels');
+      localStorage.setItem('youlagVideoLabels', 'true');
+    }
+    else {
+      document.body.classList.remove('youlag-video-labels');
+      localStorage.setItem('youlagVideoLabels', 'false');
+    }
+  }
+
 }
 
 /*****************************************
